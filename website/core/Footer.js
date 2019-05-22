a5625a73598101a88f3627ee31673bdff9de0bed
pgbarletta <pbarletta@gmail.com>
2019-05-22 09:35:01 +0200

    Minor changes on webpage

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -28,8 +28,8 @@ class Footer extends React.Component {
               <img
                 src={this.props.config.baseUrl + this.props.config.footerIcon}
                 alt={this.props.config.title}
-                width="120"
-                height="110"
+                width="68"
+                height="58"
               />
             )}
           </a>
